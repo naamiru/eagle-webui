932c2f609a126c11844be29dc92348c8ecdf84ab
naamiru <naamiru@example.com>
2025-08-08 15:41:46 +0900

    code base

---
+++ front/src/routes/__root.tsx
@@ -0,0 +1,14 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import "../styles/pico.css";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+      <ReactQueryDevtools buttonPosition="bottom-right" />
+    </>
+  ),
+});
